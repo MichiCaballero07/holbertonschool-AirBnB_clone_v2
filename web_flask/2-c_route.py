e15f7462473919227a66d8e05f3a9b51101043ad
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-08-16 16:10:51 -0500

    new file

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,28 +1,39 @@
 #!/usr/bin/python3
+"""Inicialization of the Python Flask application"""
 from flask import Flask
+
 app = Flask(__name__)
 
 
+'''Route root URL'''
+
+
 @app.route('/', strict_slashes=False)
-def app_01():
-    """create basic page
-    """
+def hello_hbnb():
+    '''Display hello World'''
     return "Hello HBNB!"
 
 
+'''Route for /hbnb'''
+
+
 @app.route('/hbnb', strict_slashes=False)
-def app_02():
-    """create a secound page
-    """
+def hbnb():
+    '''Display hello HBNB!'''
     return "HBNB"
 
 
+'''Route for /hbnb'''
+
+
 @app.route('/c/<text>', strict_slashes=False)
-def app_03(text):
-    """create C is any case
-    """
-    return "C {}".format(text.replace("_", " "))
+def c_text(text):
+    '''Display "C" followed by the value of <text>'''
+    '''Replace _ with "" in variables'''
+    formatText = text.replace('_', ' ')
+    return "C {}".format(formatText)
 
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    # start Flask server
+    app.run(debug=True, port=5000, host="0.0.0.0")
