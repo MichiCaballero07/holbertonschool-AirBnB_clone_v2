0cc44d3a340d70b85b05d4a8931a1336f292d14e
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-08-16 15:37:57 -0500

    update file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def web_flask():
-    """create a basic page
-    """
-    return "Hello HBNB!"
+    """create a basic page"""
+    return 'Hello HBNB!'
 
 """Entry Point"""
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
