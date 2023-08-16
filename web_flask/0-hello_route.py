5974eeb94e4bf6d37e3db8718c0642b7ee62d014
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-08-16 15:44:04 -0500

    update file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Import Modules"""
 from flask import Flask
+
 app = Flask(__name__)
 
 
@@ -11,4 +12,4 @@ def web_flask():
 
 """Entry Point"""
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(debug=True, port=5000, host="0.0.0.0")
