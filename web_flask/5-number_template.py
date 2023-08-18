6c1102b3283a6002b13f1408a41dffc4f908216a
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-08-18 11:30:42 -0500

    update

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,41 +1,60 @@
 #!/usr/bin/python3
-"""Import Modules"""
-from flask import Flask
+"""Initialization of the Python Flask application"""
+from flask import Flask, render_template
+
+
 app = Flask(__name__)
+
+'''Route root URL'''
 
 
 @app.route('/', strict_slashes=False)
-def hello_route():
-    """Main Route"""
-    return 'Hello HBNB!'
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+'''Route for /hbnb'''
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """hbnb Route"""
-    return 'HBNB'
+    return "HBNB"
+
+
+'''Route for /c/<text>'''
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    """c route , return text with spaces"""
-    text_with_spaces = text.replace('_', ' ')
-    return f"C {text_with_spaces}"
+def c_text(text):
+    text = text.replace('_', ' ')
+    return f"C {text}"
 
 
-@app.route('/python/', defaults={'text': 'is_cool'})
+'''Route for /python/<text> with default value "is cool"'''
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_route(text):
-    """python route, return text with spaces"""
-    return "Python {}".format(text.replace('_', ' '))
+def python_text(text):
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+'''Route for /number/<n>'''
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number_route(n):
-    """number route, return if only is an integer"""
+def number_n(n):
     return f"{n} is a number"
 
 
-"""Entry Point"""
+'''Route for /number_template/<n>'''
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template_n(n):
+    return render_template('5-number.html', number=n)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
