98b1d2d19db9f6bf5f397500cc42021cb5229ebe
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-08-18 11:33:02 -0500

    update

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,60 +1,47 @@
 #!/usr/bin/python3
-"""Initialization of the Python Flask application"""
+'''Use the module of flask'''
 from flask import Flask, render_template
-
+'''
+Initializate this application with flask
+'''
 
 app = Flask(__name__)
-
-'''Route root URL'''
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    return "Hello HBNB!"
-
-
-'''Route for /hbnb'''
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return "HBNB"
-
-
-'''Route for /c/<text>'''
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    text = text.replace('_', ' ')
-    return f"C {text}"
+def c_is_fun(text):
+    text_format = text.replace('_', ' ')
+    result = f'C {text_format}'
+    return result
 
 
-'''Route for /python/<text> with default value "is cool"'''
-
-
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text):
-    text = text.replace('_', ' ')
-    return f"Python {text}"
-
-
-'''Route for /number/<n>'''
+def python_is_cool(text='is cool'):
+    text_format = text.replace('_', ' ')
+    result = f'Python {text_format}'
+    return result
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number_n(n):
-    return f"{n} is a number"
+def number(n):
+    return f'{n} is a number'
 
 
-'''Route for /number_template/<n>'''
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template_n(n):
+@app.route('/number_template/<int:n>')
+def number_template(n):
     return render_template('5-number.html', number=n)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, port=5000, host='0.0.0.0')
